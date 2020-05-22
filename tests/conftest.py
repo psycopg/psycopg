5ab888c016997131f1cfe55b2258ddb63d048d59
Daniele Varrazzo <daniele.varrazzo@gmail.com>
2020-05-22 21:03:58 +1200

    Added a few concurrency tests

--- tests/conftest.py
+++ tests/conftest.py
@@ -2,3 +2,10 @@ pytest_plugins = (
     "tests.fix_db",
     "tests.fix_pq",
 )
+
+
+def pytest_configure(config):
+    # register slow marker
+    config.addinivalue_line(
+        "markers", "slow: this test is kinda slow (skip with -m 'not slow')"
+    )
