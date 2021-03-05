fa2cd2416aba354d9aae1d707406b1b656fa49df
Daniele Varrazzo <daniele.varrazzo@gmail.com>
2021-03-05 18:46:30 +0100

    Run leak tests with both text and binary output
    At the moment the output text is actually skipped but it will run once
    text input is no more xfailed.

--- tests/test_cursor.py
+++ tests/test_cursor.py
@@ -549,7 +549,9 @@ def test_leak(dsn, faker, fmt, fetch, row_factory):
     n = []
     for i in range(3):
         with psycopg3.connect(dsn) as conn:
-            with conn.cursor(binary=True, row_factory=row_factory) as cur:
+            with conn.cursor(
+                binary=fmt == Format.BINARY, row_factory=row_factory
+            ) as cur:
                 cur.execute(faker.drop_stmt)
                 cur.execute(faker.create_stmt)
                 cur.executemany(faker.insert_stmt, faker.records)
--- tests/test_cursor_async.py
+++ tests/test_cursor_async.py
@@ -464,7 +464,7 @@ async def test_leak(dsn, faker, fmt, fetch, row_factory):
     for i in range(3):
         async with await psycopg3.AsyncConnection.connect(dsn) as conn:
             async with conn.cursor(
-                binary=True, row_factory=row_factory
+                binary=fmt == Format.BINARY, row_factory=row_factory
             ) as cur:
                 await cur.execute(faker.drop_stmt)
                 await cur.execute(faker.create_stmt)
