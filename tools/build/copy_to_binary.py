8dc9b3b909fe8dc8c7c5d3ee175d17a1cb4b3f3e
Daniele Varrazzo <daniele.varrazzo@gmail.com>
2021-06-28 14:32:23 +0100

    Make the script to build the binary package portable

---
+++ tools/build/copy_to_binary.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python3
+
+# Create the psycopg-binary package by renaming and patching psycopg-c
+
+import os
+import re
+import shutil
+from pathlib import Path
+from typing import Union
+
+curdir = Path(__file__).parent
+pdir = curdir / "../.."
+target = pdir / "psycopg_binary"
+
+if target.exists():
+    raise Exception(f"path {target} already exists")
+
+
+def sed_i(pattern: str, repl: str, filename: Union[str, Path]) -> None:
+    with open(filename, "rb") as f:
+        data = f.read()
+    newdata = re.sub(pattern.encode("utf8"), repl.encode("utf8"), data)
+    if newdata != data:
+        with open(filename, "wb") as f:
+            f.write(newdata)
+
+
+shutil.copytree(pdir / "psycopg_c", target)
+shutil.move(str(target / "psycopg_c"), str(target / "psycopg_binary"))
+sed_i("psycopg-c", "psycopg-binary", target / "setup.cfg")
+sed_i(
+    r"__impl__\s*=.*", '__impl__ = "binary"', target / "psycopg_binary/pq.pyx"
+)
+for dirpath, dirnames, filenames in os.walk(target):
+    for filename in filenames:
+        if os.path.splitext(filename)[1] not in (".pyx", ".pxd", ".py"):
+            continue
+        sed_i(r"\bpsycopg_c\b", "psycopg_binary", Path(dirpath) / filename)
