28b60c31597019b8d5b817f3219c1f76d17b458a
Daniele Varrazzo <daniele.varrazzo@gmail.com>
2021-08-24 03:59:15 +0200

    Bump to next dev version

--- psycopg/psycopg/version.py
+++ psycopg/psycopg/version.py
@@ -6,6 +6,6 @@ psycopg distribution version file.
 
 # Use a versioning scheme as defined in
 # https://www.python.org/dev/peps/pep-0440/
-__version__ = "3.0.dev2"
+__version__ = "3.0.dev3"
 
 # also change psycopg_c/psycopg_c/version.py accordingly.
