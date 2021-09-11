451f3a0ed351e441cd1b22dec13c5b04e1a5c0e3
Jacopo Farina <jacopo.farina@flixbus.com>
2021-09-11 13:08:55 +0200

    Fix all the issues found by tox

--- psycopg/psycopg/types/geometry.py
+++ psycopg/psycopg/types/geometry.py
@@ -2,13 +2,16 @@
 Adapters for PostGIS geometries
 """
 
-import psycopg
+from typing import TYPE_CHECKING
+
 from psycopg.adapt import Dumper, Loader
 from psycopg.pq import Format
 from psycopg.types import TypeInfo
+if TYPE_CHECKING:
+    from psycopg.connection import Connection
+    from psycopg.rows import Row
 
-
-def register_shapely_adapters(conn: psycopg.Connection):
+def register_shapely_adapters(conn: Connection[Row]) -> None:
     """Register Shapely dumper and loaders.
 
     After invoking this function on a connection, the queries retrieving
@@ -26,29 +29,31 @@ def register_shapely_adapters(conn: psycopg.Connection):
     class GeometryBinaryLoader(Loader):
         format = Format.BINARY
 
-        def load(self, data):
+        def load(self, data: bytes) -> BaseGeometry:
             return shapely.wkb.loads(data)
 
     class GeometryTextLoader(Loader):
         format = Format.TEXT
 
-        def load(self, data):
+        def load(self, data: bytes) -> BaseGeometry:
             # it's a hex string in binary
             return shapely.wkb.loads(data.decode(), hex=True)
 
     class GeometryBinaryDumper(Dumper):
         format = Format.BINARY
 
-        def dump(self, obj):
-            return shapely.wkb.dumps(obj).encode()
+        def dump(self, obj: BaseGeometry) -> bytes:
+            return shapely.wkb.dumps(obj).encode() # type: ignore
 
     class GeometryTextDumper(Dumper):
         format = Format.TEXT
 
-        def dump(self, obj):
-            return shapely.wkb.dumps(obj, hex=True).encode()
+        def dump(self, obj: BaseGeometry) -> bytes:
+            return shapely.wkb.dumps(obj, hex=True).encode() # type: ignore
 
     t = TypeInfo.fetch(conn, "geometry")
+    if t is None:
+        raise ValueError("The database has no geometry type. Is it PostGIS?")
     t.register(conn)
     conn.adapters.register_loader("geometry", GeometryBinaryLoader)
     conn.adapters.register_loader("geometry", GeometryTextLoader)
