53484c3c5852ad72b851a19d6f9838a828b9148d
Daniele Varrazzo <daniele.varrazzo@gmail.com>
2021-11-08 19:14:28 +0100

    Strip only psycopg dynamic libraries, not the system ones
    Stripping symbols is beneficial (reduction of 30% of the final package, > %90%
    of the installed libraries. However just running `auditwheel repair --strip`
    breaks some of the libraries included from the system, which fail at import
    with errors such as "ELF load command address/offset not properly aligned".

---
+++ tools/build/strip_wheel.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+# Strip symbols inplace from the libraries in a zip archive.
+#
+# Stripping symbols is beneficial (reduction of 30% of the final package, >
+# %90% of the installed libraries. However just running `auditwheel repair
+# --strip` breaks some of the libraries included from the system, which fail at
+# import with errors such as "ELF load command address/offset not properly
+# aligned".
+#
+# System libraries are already pretty stripped. Ours go around 24Mb -> 1.5Mb...
+#
+# This script is designed to run on a wheel archive before auditwheel.
+
+set -euo pipefail
+set -x
+
+wheel=$(realpath "$1")
+tmpdir=$(mktemp -d)
+trap "rm -r ${tmpdir}" EXIT
+
+unzip -d "${tmpdir}" "${wheel}"
+find "${tmpdir}" -name \*.so | xargs strip
+(cd "${tmpdir}" && zip -fr ${wheel} *)
