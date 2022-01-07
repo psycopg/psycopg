5193d49ae638bc277e524e3445e994848bd18945
Daniele Varrazzo <daniele.varrazzo@gmail.com>
2022-01-07 18:32:47 +0100

    Don't overwrite the module of the psycopg.pq.pq_ctypes objects
    It might be useful to tell apart the C version from the python versions
    for debugging. The C versions don't do it, so they repr as:
    
        <psycopg_binary.pq.PGconn [IDLE] (database=piro) at 0x7f8556deb040>
    
    See #201.

--- psycopg/psycopg/pq/pq_ctypes.py
+++ psycopg/psycopg/pq/pq_ctypes.py
@@ -67,7 +67,6 @@ class PGconn:
     Python representation of a libpq connection.
     """
 
-    __module__ = "psycopg.pq"
     __slots__ = (
         "_pgconn_ptr",
         "notice_handler",
@@ -729,7 +728,6 @@ class PGresult:
     Python representation of a libpq result.
     """
 
-    __module__ = "psycopg.pq"
     __slots__ = ("_pgresult_ptr",)
 
     def __init__(self, pgresult_ptr: impl.PGresult_struct):
@@ -860,7 +858,6 @@ class PGcancel:
     Created by `PGconn.get_cancel()`.
     """
 
-    __module__ = "psycopg.pq"
     __slots__ = ("pgcancel_ptr",)
 
     def __init__(self, pgcancel_ptr: impl.PGcancel_struct):
@@ -900,8 +897,6 @@ class Conninfo:
     """
     Utility object to manipulate connection strings.
     """
-
-    __module__ = "psycopg.pq"
 
     @classmethod
     def get_defaults(cls) -> List[ConninfoOption]:
@@ -955,8 +950,6 @@ class Escaping:
     """
     Utility object to escape strings for SQL interpolation.
     """
-
-    __module__ = "psycopg.pq"
 
     def __init__(self, conn: Optional[PGconn] = None):
         self.conn = conn
