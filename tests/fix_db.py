2460a5ef326c6662954e61eb5bb7d9aed73da7d2
Daniele Varrazzo <daniele.varrazzo@gmail.com>
2022-03-27 20:52:57 +0200

    test: display the version of the server tests are run against

--- tests/fix_db.py
+++ tests/fix_db.py
@@ -27,6 +27,22 @@ def pytest_addoption(parser):
     )
 
 
+def pytest_report_header(config):
+    dsn = config.getoption("--test-dsn")
+    if dsn is None:
+        return []
+
+    try:
+        with psycopg.connect(dsn, connect_timeout=10) as conn:
+            server_version = conn.execute("select version()").fetchall()[0][0]
+    except Exception as ex:
+        server_version = f"unknown ({ex})"
+
+    return [
+        f"Server version: {server_version}",
+    ]
+
+
 def pytest_configure(config):
     # register pg marker
     config.addinivalue_line(
