489fcd7e45bfac54b4f679ac2cdfab3ee303742f
Daniele Varrazzo <daniele.varrazzo@gmail.com>
2022-11-04 23:16:24 +0100

    chore(pool): bump to next dev version number

--- psycopg_pool/psycopg_pool/version.py
+++ psycopg_pool/psycopg_pool/version.py
@@ -8,6 +8,6 @@ psycopg pool version file.
 # https://www.python.org/dev/peps/pep-0440/
 
 # STOP AND READ! if you change:
-__version__ = "3.1.4"
+__version__ = "3.1.5.dev0"
 # also change:
 # - `docs/news_pool.rst` to declare this version current or unreleased
