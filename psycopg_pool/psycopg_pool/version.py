cf74d4bc83526f98265e9f28951ff614e2fcabab
stengah <deepsghimire1@gmail.com>
2022-12-22 20:29:05 +0545

    refactor(pool): get package version from metadata

--- psycopg_pool/psycopg_pool/version.py
+++ psycopg_pool/psycopg_pool/version.py
@@ -1,6 +1,7 @@
 """
 psycopg pool version file.
 """
+from ._compat import metadata
 
 # Copyright (C) 2021 The Psycopg Team
 
@@ -8,6 +9,9 @@ psycopg pool version file.
 # https://www.python.org/dev/peps/pep-0440/
 
 # STOP AND READ! if you change:
-__version__ = "3.1.6.dev1"
+try:
+    __version__ = metadata.version("psycopg-pool")
+except metadata.PackageNotFoundError:
+    __version__ = "0.0.0.0"
 # also change:
 # - `docs/news_pool.rst` to declare this version current or unreleased
