fc54d779855a2e291a6f95518a8e79a851c6843e
Daniele Varrazzo <daniele.varrazzo@gmail.com>
2022-12-22 22:19:07 +0000

    chore(c): lean win32

--- psycopg_c/psycopg_c/_psycopg.pyx
+++ psycopg_c/psycopg_c/_psycopg.pyx
@@ -30,6 +30,7 @@ cdef extern from *:
     """
 /* Include this early to avoid a warning about redefined ARRAYSIZE in winnt.h */
 #ifdef MS_WINDOWS
+#define WIN32_LEAN_AND_MEAN
 #include <winsock2.h>
 #endif
 
