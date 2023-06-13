psycopg/psycopg

commit 9220293dc023b757f2a57702c14b1460ff8f25b0

feat: add `Error.pgconn` attribute
Might be useful to investigate errors such as bad/missing password. See #242
for details.

psycopg/psycopg/errors.py
@@ -21,7 +21,7 @@ DBAPI-defined Exceptions are defined in the following hierarchy::
 from typing import Any, Dict, Optional, Sequence, Tuple, Type, Union
 from typing import cast
 
-from psycopg.pq.abc import PGresult
+from psycopg.pq.abc import PGconn, PGresult
 from psycopg.pq._enums import DiagnosticField
 
 ErrorInfo = Union[None, PGresult, Dict[int, Optional[bytes]]]
@@ -52,13 +52,19 @@ class Error(Exception):
     __module__ = "psycopg"
 
     sqlstate: Optional[str] = None
+    pgconn: Optional[PGconn] = None
 
     def __init__(
-        self, *args: Sequence[Any], info: ErrorInfo = None, encoding: str = "utf-8"
+        self,
+        *args: Sequence[Any],
+        info: ErrorInfo = None,
+        encoding: str = "utf-8",
+        pgconn: Optional[PGconn] = None
     ):
         super().__init__(*args)
         self._info = info
         self._encoding = encoding
+        self.pgconn = pgconn
 
         # Handle sqlstate codes for which we don't have a class.
         if not self.sqlstate and info:
@@ -75,6 +81,8 @@ class Error(Exception):
         res = super().__reduce__()
         if isinstance(res, tuple) and len(res) >= 3:
             res[2]["_info"] = self._info_to_dict(self._info)
+            # To make the exception picklable
+            res[2]["pgconn"] = None
 
         return res
 

psycopg/psycopg/generators.py
@@ -39,7 +39,8 @@ def connect(conninfo: str) -> PQGenConn[PGconn]:
         if conn.status == ConnStatus.BAD:
             encoding = conninfo_encoding(conninfo)
             raise e.OperationalError(
-                f"connection is bad: {pq.error_message(conn, encoding=encoding)}"
+                f"connection is bad: {pq.error_message(conn, encoding=encoding)}",
+                pgconn=conn,
             )
 
         status = conn.connect_poll()
@@ -52,10 +53,11 @@ def connect(conninfo: str) -> PQGenConn[PGconn]:
         elif status == PollingStatus.FAILED:
             encoding = conninfo_encoding(conninfo)
             raise e.OperationalError(
-                f"connection failed: {pq.error_message(conn, encoding=encoding)}"
+                f"connection failed: {pq.error_message(conn, encoding=encoding)}",
+                pgconn=conn,
             )
         else:
-            raise e.InternalError(f"unexpected poll status: {status}")
+            raise e.InternalError(f"unexpected poll status: {status}", pgconn=conn)
 
     conn.nonblocking = 1
     return conn

psycopg_c/psycopg_c/_psycopg/generators.pyx
@@ -35,7 +35,8 @@ def connect(conninfo: str) -> PQGenConn[abc.PGconn]:
         if conn_status == libpq.CONNECTION_BAD:
             encoding = conninfo_encoding(conninfo)
             raise e.OperationalError(
-                f"connection is bad: {error_message(conn, encoding=encoding)}"
+                f"connection is bad: {error_message(conn, encoding=encoding)}",
+                pgconn=conn
             )
 
         poll_status = libpq.PQconnectPoll(pgconn_ptr)
@@ -49,10 +50,13 @@ def connect(conninfo: str) -> PQGenConn[abc.PGconn]:
         elif poll_status == libpq.PGRES_POLLING_FAILED:
             encoding = conninfo_encoding(conninfo)
             raise e.OperationalError(
-                f"connection failed: {error_message(conn, encoding=encoding)}"
+                f"connection failed: {error_message(conn, encoding=encoding)}",
+                pgconn=conn
             )
         else:
-            raise e.InternalError(f"unexpected poll status: {poll_status}")
+            raise e.InternalError(
+                f"unexpected poll status: {poll_status}", pgconn=conn
+            )
 
     conn.nonblocking = 1
     return conn

tests/test_errors.py
@@ -4,6 +4,7 @@ from weakref import ref
 
 import pytest
 
+import psycopg
 from psycopg import pq
 from psycopg import errors as e
 
@@ -260,3 +261,20 @@ def test_unknown_sqlstate(conn):
     # Survives pickling too
     pexc = pickle.loads(pickle.dumps(exc))
     assert pexc.sqlstate == code
+
+
+def test_pgconn_error():
+    with pytest.raises(psycopg.OperationalError) as excinfo:
+        psycopg.connect("dbname=nosuchdb")
+
+    exc = excinfo.value
+    assert exc.pgconn
+    assert exc.pgconn.db == b"nosuchdb"
+
+
+def test_pgconn_error_pickle():
+    with pytest.raises(psycopg.OperationalError) as excinfo:
+        psycopg.connect("dbname=nosuchdb")
+
+    exc = pickle.loads(pickle.dumps(excinfo.value))
+    assert not exc.pgconn

commit e70010fa2626291201b76112a0f4593dcd3d92e3

fix: finish the PGconn upon connection failure
Attaching the non-finished PGconn to exceptions raised in connect() is
causing problem, as described in issue #565, because the PGconn might
not get finished soon enough in application code and the socket would
remain open.

On the other hand, just removing the pgconn attribute from Error would
be a breaking change and we'd loose the inspection features introduced
in commit 9220293dc023b757f2a57702c14b1460ff8f25b0.

As an alternative, we define a new PGconn implementation that is
error-specific: it captures all attributes of the original PGconn and
fails upon other operations (methods call). Some attributes have a
default value since they are not available in old PostgreSQL versions.

Finally, the (real) PGconn is finished before raising exception in
generators.connect().

psycopg/psycopg/errors.py
@@ -18,17 +18,200 @@ DBAPI-defined Exceptions are defined in the following hierarchy::
 
 # Copyright (C) 2020 The Psycopg Team
 
-from typing import Any, Dict, Optional, Sequence, Tuple, Type, Union
+from dataclasses import dataclass, field, fields
+from typing import Any, Callable, Dict, List, NoReturn, Optional, Sequence, Tuple, Type
+from typing import Union, TYPE_CHECKING
 from typing_extensions import TypeAlias
 from asyncio import CancelledError
 
 from .pq.abc import PGconn, PGresult
-from .pq._enums import DiagnosticField
+from .pq._enums import ConnStatus, DiagnosticField, PipelineStatus, TransactionStatus
 from ._compat import TypeGuard
+
+if TYPE_CHECKING:
+    from .pq.misc import PGnotify, ConninfoOption
 
 ErrorInfo: TypeAlias = Union[None, PGresult, Dict[int, Optional[bytes]]]
 
 _sqlcodes: Dict[str, "Type[Error]"] = {}
+
+
+@dataclass
+class FinishedPGconn:
+    """Finished libpq connection.
+
+    Attributes are set from a real `~pscopg.pq.PGconn` but any operations will
+    raise an `~psycopg.OperationalError`.
+    """
+
+    info: List["ConninfoOption"] = field(default_factory=list)
+
+    db: bytes = b""
+    user: bytes = b""
+    password: bytes = b""
+    host: bytes = b""
+    hostaddr: bytes = b""
+    port: bytes = b""
+    tty: bytes = b""
+    options: bytes = b""
+    status: int = ConnStatus.BAD.value
+    transaction_status: int = TransactionStatus.UNKNOWN.value
+    pipeline_status: int = PipelineStatus.OFF.value
+
+    error_message: bytes = b""
+    server_version: int = 0
+
+    backend_pid: int = 0
+    needs_password: bool = False
+    used_password: bool = False
+    ssl_in_use: bool = False
+
+    nonblocking: int = 0
+
+    notice_handler: Optional[Callable[["PGresult"], None]] = None
+    notify_handler: Optional[Callable[["PGnotify"], None]] = None
+
+    @staticmethod
+    def _raise() -> NoReturn:
+        raise OperationalError("the connection is closed")
+
+    @classmethod
+    def connect(cls, *args: Any) -> NoReturn:
+        raise TypeError(f"{cls} is unusable")
+
+    @classmethod
+    def connect_start(cls, *args: Any) -> NoReturn:
+        raise TypeError(f"{cls} is unusable")
+
+    def connect_poll(self) -> NoReturn:
+        self._raise()
+
+    def finish(self) -> None:
+        pass
+
+    def reset(self) -> NoReturn:
+        self._raise()
+
+    def reset_start(self) -> NoReturn:
+        self._raise()
+
+    def reset_poll(self) -> NoReturn:
+        self._raise()
+
+    @classmethod
+    def ping(cls, *args: Any) -> NoReturn:
+        raise TypeError(f"{cls} is unusable")
+
+    def parameter_status(self, *args: Any) -> NoReturn:
+        self._raise()
+
+    @property
+    def socket(self) -> NoReturn:
+        self._raise()
+
+    def exec_(self, *args: Any) -> NoReturn:
+        self._raise()
+
+    def send_query(self, *args: Any) -> None:
+        self._raise()
+
+    def exec_params(self, *args: Any) -> NoReturn:
+        self._raise()
+
+    def send_query_params(self, *args: Any) -> NoReturn:
+        self._raise()
+
+    def send_prepare(self, *args: Any) -> NoReturn:
+        self._raise()
+
+    def send_query_prepared(self, *args: Any) -> NoReturn:
+        self._raise()
+
+    def prepare(self, *args: Any) -> NoReturn:
+        self._raise()
+
+    def exec_prepared(self, *args: Any) -> NoReturn:
+        self._raise()
+
+    def describe_prepared(self, *args: Any) -> NoReturn:
+        self._raise()
+
+    def send_describe_prepared(self, *args: Any) -> NoReturn:
+        self._raise()
+
+    def describe_portal(self, *args: Any) -> NoReturn:
+        self._raise()
+
+    def send_describe_portal(self, *args: Any) -> NoReturn:
+        self._raise()
+
+    def get_result(self) -> NoReturn:
+        self._raise()
+
+    def consume_input(self) -> NoReturn:
+        self._raise()
+
+    def is_busy(self) -> NoReturn:
+        self._raise()
+
+    def flush(self) -> NoReturn:
+        self._raise()
+
+    def set_single_row_mode(self) -> NoReturn:
+        self._raise()
+
+    def get_cancel(self) -> NoReturn:
+        self._raise()
+
+    def notifies(self) -> NoReturn:
+        self._raise()
+
+    def put_copy_data(self, *args: Any) -> NoReturn:
+        self._raise()
+
+    def put_copy_end(self, *args: Any) -> NoReturn:
+        self._raise()
+
+    def get_copy_data(self, *args: Any) -> NoReturn:
+        self._raise()
+
+    def trace(self, *args: Any) -> NoReturn:
+        self._raise()
+
+    def set_trace_flags(self, *args: Any) -> NoReturn:
+        self._raise()
+
+    def untrace(self) -> NoReturn:
+        self._raise()
+
+    def encrypt_password(self, *args: Any) -> NoReturn:
+        self._raise()
+
+    def make_empty_result(self, *args: Any) -> NoReturn:
+        self._raise()
+
+    def enter_pipeline_mode(self) -> NoReturn:
+        self._raise()
+
+    def exit_pipeline_mode(self) -> NoReturn:
+        self._raise()
+
+    def pipeline_sync(self) -> NoReturn:
+        self._raise()
+
+    def send_flush_request(self) -> NoReturn:
+        self._raise()
+
+
+def finish_pgconn(pgconn: PGconn) -> PGconn:
+    args = {}
+    for f in fields(FinishedPGconn):
+        try:
+            args[f.name] = getattr(pgconn, f.name)
+        except Exception:
+            pass
+    pgconn.finish()
+    return FinishedPGconn(**args)
 
 
 class Warning(Exception):

psycopg/psycopg/generators.py
@@ -76,10 +76,12 @@ def _connect(conninfo: str) -> PQGenConn[PGconn]:
             encoding = conninfo_encoding(conninfo)
             raise e.OperationalError(
                 f"connection failed: {pq.error_message(conn, encoding=encoding)}",
-                pgconn=conn,
+                pgconn=e.finish_pgconn(conn),
             )
         else:
-            raise e.InternalError(f"unexpected poll status: {status}", pgconn=conn)
+            raise e.InternalError(
+                f"unexpected poll status: {status}", pgconn=e.finish_pgconn(conn)
+            )
 
     conn.nonblocking = 1
     return conn

psycopg_c/psycopg_c/_psycopg/generators.pyx
@@ -56,11 +56,12 @@ def connect(conninfo: str) -> PQGenConn[abc.PGconn]:
             encoding = conninfo_encoding(conninfo)
             raise e.OperationalError(
                 f"connection failed: {error_message(conn, encoding=encoding)}",
-                pgconn=conn
+                pgconn=e.finish_pgconn(conn),
             )
         else:
             raise e.InternalError(
-                f"unexpected poll status: {poll_status}", pgconn=conn
+                f"unexpected poll status: {poll_status}",
+                pgconn=e.finish_pgconn(conn),
             )
 
     conn.nonblocking = 1

tests/test_errors.py
@@ -12,6 +12,18 @@ from .utils import eur, gc_collect
 from .fix_crdb import is_crdb
 
 
+def test_finishedpgconn(pgconn):
+    with pytest.raises(TypeError):
+        e.FinishedPGconn.connect()
+
+    assert pgconn.socket
+    finished = e.finish_pgconn(pgconn)
+    with pytest.raises(e.OperationalError, match="connection is closed"):
+        finished.socket
+    with pytest.raises(e.OperationalError, match="connection is closed"):
+        pgconn.socket
+
+
 @pytest.mark.crdb_skip("severity_nonlocalized")
 def test_error_diag(conn):
     cur = conn.cursor()

tests/test_generators.py
@@ -36,6 +36,12 @@ def test_connect_operationalerror_pgconn(generators, dsn, monkeypatch):
     pgconn = excinfo.value.pgconn
     assert pgconn is not None
     assert pgconn.needs_password
+    assert b"fe_sendauth: no password supplied" in pgconn.error_message
+    assert pgconn.status == pq.ConnStatus.BAD.value
+    assert pgconn.transaction_status == pq.TransactionStatus.UNKNOWN.value
+    assert pgconn.pipeline_status == pq.PipelineStatus.OFF.value
+    with pytest.raises(psycopg.OperationalError, match="connection is closed"):
+        pgconn.exec_(b"select 1")
 
 
 @pytest.fixture