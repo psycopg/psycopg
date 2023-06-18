597326a5e1ac926b67b6a39fe0c87383db244962
Daniele Varrazzo <daniele.varrazzo@gmail.com>
2023-06-18 19:18:09 +0200

    ops: fix broken build on Windows amd64

--- tools/build/wheel_win32_before_build.bat
+++ tools/build/wheel_win32_before_build.bat
@@ -1,3 +1,23 @@
 @echo on
 pip install delvewheel
-choco upgrade postgresql
+
+REM I really want to write "REM", like when I had a C=64
+REM (I am joking of course: I never wrote a comment when I had a C=64)
+
+REM Broken since 2023-05-21, Failing with the error:
+REM
+REM   postgresql (exited 1) - postgresql not installed. An error occurred during
+REM   installation: Unable to resolve dependency 'postgresql15 (= 15.3)'.
+REM
+REM Weeks later the error changed:
+REM
+REM   Unable to resolve dependency 'postgresql15': Unable to resolve
+REM   dependencies. REM   'postgresql15 15.0.1' is not compatible with
+REM   'postgresql 15.3.0 constraint: postgresql15 (= 15.3.0)'.
+REM
+REM choco upgrade postgresql
+
+REM On https://community.chocolatey.org/packages/postgresql15/15.0.1#discussion
+REM I found the following command in a comment:
+choco install postgresql15 --version 15.0.1
+REM which I'm going to randomly try.
