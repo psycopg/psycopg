fac75e476c054447b25b44fa2894bee00a2c6a5b
Daniele Varrazzo <daniele.varrazzo@gmail.com>
2023-08-07 10:11:24 +0100

    refactor(tests): auto-generate the test_cursor module from async_test_cursor
    Use a script to translate an async module to sync. The module is to be
    extended to cover the entire test suite and then possibly the rest of
    the code.

---
+++ tools/convert_async_to_sync.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# Convert all the auto-generated sync files from their async counterparts.
+
+set -euo pipefail
+
+dir="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+cd "${dir}/.."
+
+python "${dir}/async_to_sync.py" tests/test_cursor_async.py > tests/test_cursor.py
+black -q tests/test_cursor.py
