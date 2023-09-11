70e1f469ec5305e389935cf62c424cec2653c0ef
Daniele Varrazzo <daniele.varrazzo@gmail.com>
2023-09-11 23:19:22 +0100

    refactor(tests): rename cursor tests to keep them together
    Also rename test_default_cursor to just test_cursor as we renamed the
    common tests in the previous commit.

--- tools/convert_async_to_sync.sh
+++ tools/convert_async_to_sync.sh
@@ -23,15 +23,15 @@ for async in \
     psycopg_pool/psycopg_pool/sched_async.py \
     tests/pool/test_pool_async.py \
     tests/pool/test_sched_async.py \
-    tests/test_client_cursor_async.py \
     tests/test_connection_async.py \
     tests/test_copy_async.py \
+    tests/test_cursor_async.py \
+    tests/test_cursor_client_async.py \
     tests/test_cursor_common_async.py \
-    tests/test_default_cursor_async.py \
+    tests/test_cursor_raw_async.py \
+    tests/test_cursor_server_async.py \
     tests/test_pipeline_async.py \
     tests/test_prepared_async.py \
-    tests/test_raw_cursor_async.py \
-    tests/test_server_cursor_async.py \
     tests/test_tpc_async.py \
     tests/test_transaction_async.py
 do
