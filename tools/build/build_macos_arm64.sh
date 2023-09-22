3be33c6389a42e939835abc3df8c6176ebf4bec6
Daniele Varrazzo <daniele.varrazzo@gmail.com>
2023-09-22 23:31:46 +0200

    chore: upgrade libpq, OpenSSL versions

--- tools/build/build_macos_arm64.sh
+++ tools/build/build_macos_arm64.sh
@@ -11,7 +11,7 @@ set -euo pipefail
 set -x
 
 python_versions="3.8.10 3.9.13 3.10.5 3.11.0"
-pg_version=15
+pg_version=16
 
 # Move to the root of the project
 dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
