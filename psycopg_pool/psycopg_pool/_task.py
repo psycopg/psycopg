2fa8a00e7fcd12e93848113d903948f7a3549c40
Daniele Varrazzo <daniele.varrazzo@gmail.com>
2023-10-08 11:45:28 +0200

    refactor(pool): define scheduler task as dataclass

--- psycopg_pool/psycopg_pool/_task.py
+++ psycopg_pool/psycopg_pool/_task.py
@@ -4,24 +4,11 @@ Task for Scheduler and AsyncScheduler
 
 # Copyright (C) 2023 The Psycopg Team
 
-from typing import Any, Callable, Optional, NamedTuple
+from typing import Any, Callable, Optional
+from dataclasses import dataclass, field
 
 
-class Task(NamedTuple):
+@dataclass(order=True)
+class Task:
     time: float
-    action: Optional[Callable[[], Any]]
-
-    def __eq__(self, other: "Task") -> Any:  # type: ignore[override]
-        return self.time == other.time
-
-    def __lt__(self, other: "Task") -> Any:  # type: ignore[override]
-        return self.time < other.time
-
-    def __le__(self, other: "Task") -> Any:  # type: ignore[override]
-        return self.time <= other.time
-
-    def __gt__(self, other: "Task") -> Any:  # type: ignore[override]
-        return self.time > other.time
-
-    def __ge__(self, other: "Task") -> Any:  # type: ignore[override]
-        return self.time >= other.time
+    action: Optional[Callable[[], Any]] = field(compare=False)
