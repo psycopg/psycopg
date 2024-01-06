2583a6fb3431610c24247a4190272ba10eb8ea13
Daniele Varrazzo <daniele.varrazzo@gmail.com>
2024-01-06 17:24:48 +0100

    fix: perform multiple attemps if a host name resolve to multiple hosts
    We now perform DNS resolution in Python both in the sync and async code.
    Because the two code paths are now very similar, make sure they pass the
    same tests.
    
    Close #699.

--- tests/test_connection.py
+++ tests/test_connection.py
@@ -430,17 +430,19 @@ def test_autocommit_unknown(conn):
     [
         ((), {}, ""),
         (("",), {}, ""),
-        (("dbname=foo user=bar",), {}, "dbname=foo user=bar"),
-        (("dbname=foo",), {"user": "baz"}, "dbname=foo user=baz"),
+        (("host=foo.com user=bar",), {}, "host=foo.com user=bar hostaddr=1.1.1.1"),
+        (("host=foo.com",), {"user": "baz"}, "host=foo.com user=baz hostaddr=1.1.1.1"),
         (
             ("dbname=foo port=5433",),
             {"dbname": "qux", "user": "joe"},
             "dbname=qux user=joe port=5433",
         ),
-        (("dbname=foo",), {"user": None}, "dbname=foo"),
+        (("host=foo.com",), {"user": None}, "host=foo.com hostaddr=1.1.1.1"),
     ],
 )
-def test_connect_args(conn_cls, monkeypatch, setpgenv, pgconn, args, kwargs, want):
+def test_connect_args(
+    conn_cls, monkeypatch, setpgenv, pgconn, fake_resolve, args, kwargs, want
+):
     got_conninfo: str
 
     def fake_connect(conninfo):
@@ -857,3 +859,19 @@ def test_connect_context_copy(conn_cls, dsn, conn):
 def test_cancel_closed(conn):
     conn.close()
     conn.cancel()
+
+
+def test_resolve_hostaddr_conn(conn_cls, monkeypatch, fake_resolve):
+    got = ""
+
+    def fake_connect_gen(conninfo, **kwargs):
+        nonlocal got
+        got = conninfo
+        1 / 0
+
+    monkeypatch.setattr(conn_cls, "_connect_gen", fake_connect_gen)
+
+    with pytest.raises(ZeroDivisionError):
+        conn_cls.connect("host=foo.com")
+
+    assert conninfo_to_dict(got) == {"host": "foo.com", "hostaddr": "1.1.1.1"}
