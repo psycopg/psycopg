5e2fec0a3ee4fbe4cc855c5f9e6d2e856ec450be
Daniele Varrazzo <daniele.varrazzo@gmail.com>
2024-01-06 18:06:54 +0100

    refactor: generate conninfo attempts from async counterpart

--- tools/async_to_sync.py
+++ tools/async_to_sync.py
@@ -29,6 +29,7 @@ import ast_comments as ast
 PYVER = "3.11"
 
 ALL_INPUTS = """
+    psycopg/psycopg/_conninfo_attempts_async.py
     psycopg/psycopg/_copy_async.py
     psycopg/psycopg/connection_async.py
     psycopg/psycopg/cursor_async.py
@@ -40,6 +41,7 @@ ALL_INPUTS = """
     tests/pool/test_pool_null_async.py
     tests/pool/test_sched_async.py
     tests/test_connection_async.py
+    tests/test_conninfo_attempts_async.py
     tests/test_copy_async.py
     tests/test_cursor_async.py
     tests/test_cursor_client_async.py
