8b2e7b14f1bfa8d4e19c1ea49af881ef50ea0508
Trim21 <trim21.me@gmail.com>
2024-12-16 21:10:33 +0800

    build: remove duplicated setup command and setup

--- tools/build/wheel_win32_before_build.bat
+++ tools/build/wheel_win32_before_build.bat
@@ -1,32 +1,3 @@
 @echo on
-pip install delvewheel
 
-REM I really want to write "REM", like when I had a C=64
-REM (I am joking of course: I never wrote a comment when I had a C=64)
-
-REM Broken since 2023-05-21, Failing with the error:
-REM
-REM   postgresql (exited 1) - postgresql not installed. An error occurred during
-REM   installation: Unable to resolve dependency 'postgresql15 (= 15.3)'.
-REM
-REM Weeks later the error changed:
-REM
-REM   Unable to resolve dependency 'postgresql15': Unable to resolve
-REM   dependencies. REM   'postgresql15 15.0.1' is not compatible with
-REM   'postgresql 15.3.0 constraint: postgresql15 (= 15.3.0)'.
-REM
-REM choco upgrade postgresql
-
-REM On https://community.chocolatey.org/packages/postgresql15/15.0.1#discussion
-REM I found the following command in a comment:
-
-vcpkg --version >nul 2>&1 && (
-    vcpkg install libpq:x64-windows-release
-) || (
-    choco install postgresql15 --version 15.0.1
-)
-
-REM which I'm going to randomly try.
-
-REM See https://community.chocolatey.org/packages/postgresql15#install
-REM for the last package available (bump the version number in the url).
+vcpkg install libpq:x64-windows-release
