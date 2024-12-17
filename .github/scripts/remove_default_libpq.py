b37fdb84a45b60f0b0422b716065177ef090dea8
Trim21 <trim21.me@gmail.com>
2024-12-17 11:34:40 +0800

    ci: search dll from ctypes

--- .github/scripts/remove_default_libpq.py
+++ .github/scripts/remove_default_libpq.py
@@ -1,16 +1,13 @@
+import ctypes.util
 import os
 from pathlib import Path
 
 IS_CI = bool(os.environ.get("CI"))
 
-for path in os.environ["PATH"].split(os.pathsep):
-    p = Path(path)
-    if not p.exists():
-        continue
-
-    for file in p.iterdir():
-        if file.name == "libpq.dll":
-            print(file)
-            # only remove this in CI to avoid someone run it on their machine
-            if IS_CI:
-                file.unlink()
+while libname := ctypes.util.find_library("libpq.dll"):
+    if libname:
+        dll_file = Path(libname).resolve()
+        print(dll_file)
+        # only remove this in CI to avoid someone run it on their machine
+        if IS_CI:
+            dll_file.unlink()
