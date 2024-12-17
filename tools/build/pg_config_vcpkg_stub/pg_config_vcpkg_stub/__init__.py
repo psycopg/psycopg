4b2ee3a868070e9084c7ee4e573aa51e3d53ed9a
Trim21 <trim21.me@gmail.com>
2024-12-18 02:12:32 +0800

    add more error message in pg_config stub

--- tools/build/pg_config_vcpkg_stub/pg_config_vcpkg_stub/__init__.py
+++ tools/build/pg_config_vcpkg_stub/pg_config_vcpkg_stub/__init__.py
@@ -25,17 +25,26 @@ def main():
         return
 
     # on github actions it's `VCPKG_INSTALLATION_ROOT`
-    if "VCPKG_ROOT" in os.environ or "VCPKG_INSTALLATION_ROOT" in os.environ:
-        vcpkg_root = pathlib.Path(
-            os.environ.get("VCPKG_ROOT") or os.environ["VCPKG_INSTALLATION_ROOT"]
-        )
-        vcpkg_platform_root = vcpkg_root.joinpath(
-            "installed/x64-windows-release"
-        ).resolve()
-        if vcpkg_platform_root.exists():
-            if what == "--libdir":
-                if vcpkg_platform_root.joinpath("lib/libpq.lib").exists():
-                    print(str(vcpkg_platform_root.joinpath("lib")))
-            if what == "--includedir":
-                if vcpkg_platform_root.joinpath("include/libpq").exists():
-                    print(str(vcpkg_platform_root.joinpath("include")))
+    if "VCPKG_ROOT" not in os.environ:
+        print("failed to find VCPKG ROOT path", file=sys.stderr)
+        sys.exit(1)
+
+    vcpkg_root = pathlib.Path(os.environ["VCPKG_ROOT"])
+    vcpkg_platform_root = vcpkg_root.joinpath("installed/x64-windows-release").resolve()
+    if vcpkg_platform_root.exists():
+        if what == "--libdir":
+            if vcpkg_platform_root.joinpath("lib/libpq.lib").exists():
+                print(str(vcpkg_platform_root.joinpath("lib")))
+                return
+        if what == "--includedir":
+            if vcpkg_platform_root.joinpath("include/libpq").exists():
+                print(str(vcpkg_platform_root.joinpath("include")))
+                return
+
+    print(
+        "unexpected command: {!r}\n this maybe out-of-sync between 'psycopg_c/setup.py' and 'tools/build/pg_config_vcpkg_stub/pg_config_vcpkg_stub/__init__.py'".format(
+            sys.argv[1:]
+        ),
+        file=sys.stderr,
+    )
+    sys.exit(1)
