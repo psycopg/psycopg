d8e6642fbb3c41d8907534dba158d552729ec1c5
stankudrow <stankudrow@reply.no>
2024-12-24 11:52:59 +0300

    quote [psycopg.]sql classes for types

--- psycopg/psycopg/abc.py
+++ psycopg/psycopg/abc.py
@@ -26,7 +26,7 @@ NoneType: type = type(None)
 # An object implementing the buffer protocol
 Buffer = Union[bytes, bytearray, memoryview]
 
-Query: TypeAlias = Union[LiteralString, bytes, sql.SQL, sql.Composed]
+Query: TypeAlias = Union[LiteralString, bytes, "sql.SQL", "sql.Composed"]
 Params: TypeAlias = Union[Sequence[Any], Mapping[str, Any]]
 ConnectionType = TypeVar("ConnectionType", bound="BaseConnection[Any]")
 PipelineCommand: TypeAlias = Callable[[], None]
