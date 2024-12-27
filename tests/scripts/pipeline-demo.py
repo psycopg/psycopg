137b51f85cd95d796d242541921fc057e181e19f
Daniele Varrazzo <daniele.varrazzo@gmail.com>
2024-12-27 03:30:28 +0100

    lint: add typing annotations to pass pre-commit
    Pre-commit seems to run mypy in parallel batches, each with about 20
    files. In this mode, certain tests that pass when running mypy all in
    once or file-by-file, fail.
    
    In the current state, both running mypy without arguments or running it
    in pre-commit seem to work.

--- tests/scripts/pipeline-demo.py
+++ tests/scripts/pipeline-demo.py
@@ -190,10 +190,7 @@ def pipeline_demo_pq(rows_to_send: int, logger: logging.Logger) -> None:
     ):
         while results_queue:
             fetched = waiting.wait(
-                pipeline_communicate(
-                    pgconn,  # type: ignore[arg-type]
-                    commands,
-                ),
+                pipeline_communicate(pgconn, commands),
                 pgconn.socket,
             )
             assert not commands, commands
@@ -216,10 +213,7 @@ async def pipeline_demo_pq_async(rows_to_send: int, logger: logging.Logger) -> N
     ):
         while results_queue:
             fetched = await waiting.wait_async(
-                pipeline_communicate(
-                    pgconn,  # type: ignore[arg-type]
-                    commands,
-                ),
+                pipeline_communicate(pgconn, commands),
                 pgconn.socket,
             )
             assert not commands, commands
