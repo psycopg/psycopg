342b0db77310eba7323d46f73c13f22c92d48763
henadzit <henadzi.tsaryk@gmail.com>
2025-01-30 00:32:17 +0100

    perf(c): use UUID.__new__

--- psycopg_c/psycopg_c/types/uuid.pyx
+++ psycopg_c/psycopg_c/types/uuid.pyx
@@ -8,12 +8,6 @@ cdef extern from "Python.h":
     # support 3.0.x
     const char *PyUnicode_AsUTF8(object unicode) except NULL
 
-from libc.stdio cimport printf
-
-
-#cdef extern from "Python.h":
-#    Py_ssize_t PyLong_AsNativeBytes(PyObject* vv, void* buffer, Py_ssize_t n, int flags)
-
 
 @cython.final
 cdef class UUIDDumper(CDumper):
@@ -37,10 +31,6 @@ cdef class UUIDBinaryDumper(CDumper):
         cdef char *buf = CDumper.ensure_size(rv, offset, 16)
         memcpy(buf, src, 16)
         return 16
-        #cdef PyObject *pyobj = <PyObject*>obj.int
-        #cdef char *buf = CDumper.ensure_size(rv, offset, 16)
-        #PyLong_AsNativeBytes(pyobj, buf, 16, 4)
-        #return 16
 
 
 @cython.final
@@ -48,7 +38,20 @@ cdef class UUIDLoader(CLoader):
     format = PQ_TEXT
 
     cdef object cload(self, const char *data, size_t length):
-        return uuid.UUID(hex=data[:length].decode())
+        cdef char[33] hex_str
+        cdef size_t i
+        cdef int j = 0
+        for i in range(36):
+            if data[i] == b'-':
+                continue
+            hex_str[j] = data[i]
+            j += 1
+        hex_str[32] = 0
+
+        u = uuid.UUID.__new__(uuid.UUID)
+        object.__setattr__(u, 'is_safe', uuid.SafeUUID.unknown)
+        object.__setattr__(u, 'int', PyLong_FromString(hex_str, NULL, 16))
+        return u
 
 
 @cython.final
@@ -56,4 +59,7 @@ cdef class UUIDBinaryLoader(CLoader):
     format = PQ_BINARY
 
     cdef object cload(self, const char *data, size_t length):
-        return uuid.UUID(bytes=data[:length])
+        u = uuid.UUID.__new__(uuid.UUID)
+        object.__setattr__(u, 'is_safe', uuid.SafeUUID.unknown)
+        object.__setattr__(u, 'int', int.from_bytes(data[:length], 'big'))
+        return u
