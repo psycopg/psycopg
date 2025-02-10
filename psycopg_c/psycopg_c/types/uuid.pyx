83f8e23a7fc498c6c081e4a197a8f2c9496f3573
henadzit <henadzi.tsaryk@gmail.com>
2025-02-10 23:37:50 +0100

    chore(c): remove C UUIDDumper and UUIDBinaryDumper

--- psycopg_c/psycopg_c/types/uuid.pyx
+++ psycopg_c/psycopg_c/types/uuid.pyx
@@ -1,37 +1,5 @@
 cimport cython
-
-from types import ModuleType
-from cpython.bytes cimport PyBytes_AsString
 from cpython.long cimport PyLong_FromUnsignedLongLong
-
-cdef extern from "Python.h":
-    # PyUnicode_AsUTF8 was added to cpython.unicode in 3.1.x but we still
-    # support 3.0.x
-    const char *PyUnicode_AsUTF8(object unicode) except NULL
-
-
-@cython.final
-cdef class UUIDDumper(CDumper):
-    format = PQ_TEXT
-    oid = oids.UUID_OID
-
-    cdef Py_ssize_t cdump(self, obj, bytearray rv, Py_ssize_t offset) except -1:
-        cdef const char *src = PyUnicode_AsUTF8(obj.hex)
-        cdef char *buf = CDumper.ensure_size(rv, offset, 32)
-        memcpy(buf, src, 32)
-        return 32
-
-
-@cython.final
-cdef class UUIDBinaryDumper(CDumper):
-    format = PQ_BINARY
-    oid = oids.UUID_OID
-
-    cdef Py_ssize_t cdump(self, obj, bytearray rv, Py_ssize_t offset) except -1:
-        cdef const char *src = PyBytes_AsString(obj.bytes)
-        cdef char *buf = CDumper.ensure_size(rv, offset, 16)
-        memcpy(buf, src, 16)
-        return 16
 
 
 cdef extern from *:
