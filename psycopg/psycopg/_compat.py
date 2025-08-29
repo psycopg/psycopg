ff033e43c24cee78cdc790dac396ca1ff4017125
Daniele Varrazzo <daniele.varrazzo@gmail.com>
2025-08-29 18:52:42 +0200

    chore(test): drop use of asyncio event loop policy
    Deprecated since Python 3.14

--- psycopg/psycopg/_compat.py
+++ psycopg/psycopg/_compat.py
@@ -42,6 +42,17 @@ if sys.version_info >= (3, 11):
 else:
     from typing_extensions import LiteralString, Self
 
+if sys.version_info >= (3, 12):
+    _asyncio_run_snippet = (
+        "running 'asyncio.run(...,"
+        " loop_factory=asyncio.SelectorEventLoop(selectors.SelectSelector()))'"
+    )
+
+else:
+    _asyncio_run_snippet = (
+        "setting 'asyncio.set_event_loop_policy(WindowsSelectorEventLoopPolicy())'"
+    )
+
 if sys.version_info >= (3, 13):
     from typing import TypeVar
 else:
