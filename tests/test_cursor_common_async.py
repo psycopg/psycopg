f196925e5742742d2f66d507878294594edbb6bd
Aditya Toshniwal <aditya.toshniwal@enterprisedb.com>
2025-10-06 14:28:56 +0530

    feat: add support for setting cursor result set position for multi-result queries

--- tests/test_cursor_common_async.py
+++ tests/test_cursor_common_async.py
@@ -218,6 +218,20 @@ async def test_execute_many_results(aconn):
     assert cur.rowcount == 3
     assert cur.nextset() is None
 
+    cur.set_result(0)
+    assert cur.fetchall() == [("foo",)]
+    assert cur.rowcount == 1
+
+    cur.set_result(-1)
+    assert cur.fetchall() == [(1,), (2,), (3,)]
+    assert cur.rowcount == 3
+
+    with pytest.raises(ValueError):
+        cur.set_result(2)
+
+    with pytest.raises(ValueError):
+        cur.set_result(-3)
+
     await cur.close()
     assert cur.nextset() is None
 
