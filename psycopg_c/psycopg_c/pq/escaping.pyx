psycopg/psycopg

commit cec696609b334aa32f871bd342e39b431fc799db

feat: add get_error_message() methods
The method is available on PGconn, PGconnCancel, PGresult.

The generic function error_message() is retained only because it was
documented so we don't want to drop it. Internally only the
get_error_message() method is used.

psycopg_c/psycopg_c/pq/escaping.pyx
@@ -27,7 +27,7 @@ cdef class Escaping:
         out = libpq.PQescapeLiteral(self.conn._pgconn_ptr, ptr, length)
         if out is NULL:
             raise e.OperationalError(
-                f"escape_literal failed: {error_message(self.conn)}"
+                f"escape_literal failed: {self.conn.get_error_message())}"
             )
 
         rv = out[:strlen(out)]
@@ -49,7 +49,7 @@ cdef class Escaping:
         out = libpq.PQescapeIdentifier(self.conn._pgconn_ptr, ptr, length)
         if out is NULL:
             raise e.OperationalError(
-                f"escape_identifier failed: {error_message(self.conn)}"
+                f"escape_identifier failed: {self.conn.get_error_message()}"
             )
 
         rv = out[:strlen(out)]
@@ -76,7 +76,7 @@ cdef class Escaping:
             if error:
                 PyMem_Free(buf_out)
                 raise e.OperationalError(
-                    f"escape_string failed: {error_message(self.conn)}"
+                    f"escape_string failed: {self.conn.get_error_message()}"
                 )
 
         else:

psycopg/psycopg/_connection_base.py
@@ -26,7 +26,6 @@ from ._enums import IsolationLevel
 from ._compat import LiteralString, Self, TypeAlias, TypeVar
 from .pq.misc import connection_summary
 from ._pipeline import BasePipeline
-from ._encodings import pgconn_encoding
 from ._preparing import PrepareManager
 from ._connection_info import ConnectionInfo
 
@@ -343,7 +342,7 @@ class BaseConnection(Generic[Row]):
         if not (self and self._notice_handlers):
             return
 
-        diag = e.Diagnostic(res, pgconn_encoding(self.pgconn))
+        diag = e.Diagnostic(res, self.pgconn._encoding)
         for cb in self._notice_handlers:
             try:
                 cb(diag)
@@ -376,7 +375,7 @@ class BaseConnection(Generic[Row]):
         if not (self and self._notify_handlers):
             return
 
-        enc = pgconn_encoding(self.pgconn)
+        enc = self.pgconn._encoding
         n = Notify(pgn.relname.decode(enc), pgn.extra.decode(enc), pgn.be_pid)
         for cb in self._notify_handlers:
             cb(n)
@@ -447,7 +446,7 @@ class BaseConnection(Generic[Row]):
         self._check_connection_ok()
 
         if isinstance(command, str):
-            command = command.encode(pgconn_encoding(self.pgconn))
+            command = command.encode(self.pgconn._encoding)
         elif isinstance(command, Composable):
             command = command.as_bytes(self)
 
@@ -473,7 +472,7 @@ class BaseConnection(Generic[Row]):
         result = (yield from generators.execute(self.pgconn))[-1]
         if result.status != COMMAND_OK and result.status != TUPLES_OK:
             if result.status == FATAL_ERROR:
-                raise e.error_from_result(result, encoding=pgconn_encoding(self.pgconn))
+                raise e.error_from_result(result, encoding=self.pgconn._encoding)
             else:
                 raise e.InterfaceError(
                     f"unexpected result {pq.ExecStatus(result.status).name}"
@@ -513,7 +512,7 @@ class BaseConnection(Generic[Row]):
         result = (yield from generators.execute(self.pgconn))[-1]
         if result.status != COMMAND_OK:
             if result.status == FATAL_ERROR:
-                raise e.error_from_result(result, encoding=pgconn_encoding(self.pgconn))
+                raise e.error_from_result(result, encoding=self.pgconn._encoding)
             else:
                 raise e.InterfaceError(
                     f"unexpected result {pq.ExecStatus(result.status).name}"

psycopg/psycopg/_connection_info.py
@@ -11,7 +11,6 @@ from datetime import tzinfo
 
 from . import pq
 from ._tz import get_tzinfo
-from ._encodings import pgconn_encoding
 from .conninfo import make_conninfo
 
 
@@ -167,7 +166,7 @@ class ConnectionInfo:
     @property
     def encoding(self) -> str:
         """The Python codec name of the connection's client encoding."""
-        return pgconn_encoding(self.pgconn)
+        return self.pgconn._encoding
 
     def _get_pgconn_attr(self, name: str) -> str:
         value: bytes = getattr(self.pgconn, name)

psycopg/psycopg/_copy.py
@@ -18,7 +18,6 @@ from . import errors as e
 from ._compat import Self
 from ._copy_base import BaseCopy, MAX_BUFFER_SIZE, QUEUE_SIZE, PREFER_FLUSH
 from .generators import copy_to, copy_end
-from ._encodings import pgconn_encoding
 from ._acompat import spawn, gather, Queue, Worker
 
 if TYPE_CHECKING:
@@ -214,7 +213,7 @@ class LibpqWriter(Writer):
         bmsg: bytes | None
         if exc:
             msg = f"error from Python: {type(exc).__qualname__} - {exc}"
-            bmsg = msg.encode(pgconn_encoding(self._pgconn), "replace")
+            bmsg = msg.encode(self._pgconn._encoding, "replace")
         else:
             bmsg = None
 

psycopg/psycopg/_copy_async.py
@@ -15,7 +15,6 @@ from . import errors as e
 from ._compat import Self
 from ._copy_base import BaseCopy, MAX_BUFFER_SIZE, QUEUE_SIZE, PREFER_FLUSH
 from .generators import copy_to, copy_end
-from ._encodings import pgconn_encoding
 from ._acompat import aspawn, agather, AQueue, AWorker
 
 if TYPE_CHECKING:
@@ -213,7 +212,7 @@ class AsyncLibpqWriter(AsyncWriter):
         bmsg: bytes | None
         if exc:
             msg = f"error from Python: {type(exc).__qualname__} - {exc}"
-            bmsg = msg.encode(pgconn_encoding(self._pgconn), "replace")
+            bmsg = msg.encode(self._pgconn._encoding, "replace")
         else:
             bmsg = None
 

psycopg/psycopg/_copy_base.py
@@ -18,7 +18,6 @@ from . import errors as e
 from .abc import Buffer, ConnectionType, PQGen, Transformer
 from .pq.misc import connection_summary
 from ._cmodule import _psycopg
-from ._encodings import pgconn_encoding
 from .generators import copy_from
 
 if TYPE_CHECKING:
@@ -101,7 +100,7 @@ class BaseCopy(Generic[ConnectionType]):
         if binary:
             self.formatter = BinaryFormatter(tx)
         else:
-            self.formatter = TextFormatter(tx, encoding=pgconn_encoding(self._pgconn))
+            self.formatter = TextFormatter(tx, encoding=self._pgconn._encoding)
 
         self._finished = False
 

psycopg/psycopg/_cursor_base.py
@@ -18,7 +18,6 @@ from .rows import Row, RowMaker
 from ._column import Column
 from .pq.misc import connection_summary
 from ._queries import PostgresQuery, PostgresClientQuery
-from ._encodings import pgconn_encoding
 from ._preparing import Prepare
 from .generators import execute, fetch, send
 
@@ -615,4 +614,4 @@ class BaseCursor(Generic[ConnectionType, Row]):
 
     @property
     def _encoding(self) -> str:
-        return pgconn_encoding(self._pgconn)
+        return self._pgconn._encoding

psycopg/psycopg/_encodings.py
@@ -16,7 +16,6 @@ from .errors import NotSupportedError
 from ._compat import cache
 
 if TYPE_CHECKING:
-    from .pq.abc import PGconn
     from ._connection_base import BaseConnection
 
 OK = ConnStatus.OK
@@ -86,23 +85,7 @@ def conn_encoding(conn: BaseConnection[Any] | None) -> str:
 
     Default to utf8 if the connection has no encoding info.
     """
-    if conn:
-        return pgconn_encoding(conn.pgconn)
-    else:
-        return "utf-8"
-
-
-def pgconn_encoding(pgconn: PGconn) -> str:
-    """
-    Return the Python encoding name of a libpq connection.
-
-    Default to utf8 if the connection has no encoding info.
-    """
-    if pgconn.status == OK:
-        pgenc = pgconn.parameter_status(b"client_encoding") or b"UTF8"
-        return pg2pyenc(pgenc)
-    else:
-        return "utf-8"
+    return conn.pgconn._encoding if conn else "utf-8"
 
 
 def conninfo_encoding(conninfo: str) -> str:

commit 70cc1d1b14ab99be2a066032dc353e7c7aa77b20

fix(c): fix error revealed by Cython 3.2
The error was likely introduced in cec696609b334aa32f871bd342e39b431fc799db
but the Cython compiler didn't flag it as error.

psycopg_c/psycopg_c/pq/escaping.pyx
@@ -27,7 +27,7 @@ cdef class Escaping:
         out = libpq.PQescapeLiteral(self.conn._pgconn_ptr, ptr, length)
         if out is NULL:
             raise e.OperationalError(
-                f"escape_literal failed: {self.conn.get_error_message())}"
+                f"escape_literal failed: {self.conn.get_error_message()}"
             )
 
         rv = out[:strlen(out)]