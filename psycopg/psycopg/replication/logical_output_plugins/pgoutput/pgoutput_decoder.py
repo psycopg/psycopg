494eeed13acc59019dce3e766f87c31cac4fe108
Dylan Young <dylanyoungmeijer@gmail.com>
2026-04-25 13:10:57 -0300

    TEMP: Get real messages

--- psycopg/psycopg/replication/logical_output_plugins/pgoutput/pgoutput_decoder.py
+++ psycopg/psycopg/replication/logical_output_plugins/pgoutput/pgoutput_decoder.py
@@ -82,6 +82,73 @@ class PgOutputDecoder(
             raise e.DataError(
                 f"Unexpected message type for pgoutput plugin: '{msg_type}'"
             ) from err
+        if self.format == pq.Format.TEXT:
+            filter_initialized = lambda d: {
+                key: value for key, value in d if key != "_initialized"
+            }
+            file = "./pgoutput_message_examples.json"
+            try:
+                with open(file, "x") as f:
+                    pass
+            except FileExistsError:
+                pass
+            with open(file, "r+") as f:
+                # import pdb
+                import os
+                import json
+                import base64
+                from dataclasses import asdict
+
+                # pdb.set_trace()
+                if not f.read().strip():
+                    msg_examples = {}
+                else:
+                    f.seek(0)
+                    msg_examples = json.load(f)
+
+                if (
+                    streaming_opt := self.plugin_options.get("streaming", "off")
+                ) == "parallel":
+                    streaming_key = "example_msgs_stream_parallel"
+                elif streaming_opt == "on":
+                    streaming_key = "example_msgs_stream"
+                else:
+                    streaming_key = "example_msgs"
+                if msg_type not in msg_examples.setdefault(
+                    streaming_key, {}
+                ).setdefault("msgs", {}):
+                    msg_examples[streaming_key]["msgs"][msg_type] = base64.b64encode(
+                        payload
+                    ).decode()
+                    msg_examples[streaming_key].setdefault("streaming_xid", {})[
+                        msg_type
+                    ] = self.streaming
+                    if msg_type in "IUDT":
+                        msg_examples[streaming_key].setdefault("relations", {}).update(
+                            {
+                                rel_id: asdict(val, dict_factory=filter_initialized)
+                                for rel_id, val in self.relations.items()
+                            }
+                        )
+                        for xid in self.relations_by_xid:
+                            for subxid in self.relations_by_xid[xid]:
+                                msg_examples[streaming_key].setdefault(
+                                    "relations_by_xid", {}
+                                ).setdefault(xid, {}).update(
+                                    {
+                                        rel_id: asdict(
+                                            val, dict_factory=filter_initialized
+                                        )
+                                        for rel_id, val in self.relations_by_xid[
+                                            xid
+                                        ].items()
+                                    }
+                                )
+                f.seek(0)
+                json.dump(msg_examples, f, indent=4)
+                f.flush()
+                os.fsync(f)
+
         return decode(self, payload[1:])
 
     def get_relation(self, relation_id: int) -> RelationMessage:
